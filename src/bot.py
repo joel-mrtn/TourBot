d3fc64f3427a3c2b0ad2ac0d7d2c31602a49416d
Joel Martin <joel.acc@pm.me>
2023-04-22 21:00:57 +0200

    add guild id to code

--- src/bot.py
+++ src/bot.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+MY_GUILD = discord.Object(id=os.getenv('MY_GUILD'))
 
 
 class BotClient(discord.Client):
@@ -14,10 +15,13 @@ class BotClient(discord.Client):
         super().__init__(intents=intents)
         self.tree = app_commands.CommandTree(self)
 
+    # By specifically naming the guild, the commands are updated faster on the server.
+    async def setup_hook(self):
+        self.tree.copy_global_to(guild=MY_GUILD)
+        await self.tree.sync(guild=MY_GUILD)
+
 
 intents = discord.Intents.default()
-intents.message_content = True
-
 client = BotClient(intents=intents)
 
 
@@ -25,7 +29,7 @@ client = BotClient(intents=intents)
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
-@client.tree.command(name='hello')
+@client.tree.command(description='Get a nice greeting from the bot.')
 async def hello(interaction: discord.Integration):
     await interaction.response.send_message(f'Hey {interaction.user.mention}! Nice to see you.')
 
