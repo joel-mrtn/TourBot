b256e8a8f4c78ff209f4ee7dd471b643343fb2d2
Joel Martin <joel.acc@pm.me>
2023-04-22 20:26:36 +0200

    add command hello to client

--- src/bot.py
+++ src/bot.py
@@ -1,28 +1,33 @@
 import os
 
 import discord
+from discord import app_commands
 from dotenv import load_dotenv
 
-# get token from .env file
+
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+
+
+class BotClient(discord.Client):
+    def __init__(self, *, intents: discord.Intents):
+        super().__init__(intents=intents)
+        self.tree = app_commands.CommandTree(self)
+
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+client = BotClient(intents=intents)
+
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
-@client.event
-async def on_message(message):
-    # ignore self messages
-    if message.author == client.user:
-        return
-    
-    if message.content == 'foo':
-        await message.channel.send('bar')
+@client.tree.command(name='hello')
+async def hello(interaction: discord.Integration):
+    await interaction.response.send_message(f'Hey {interaction.user.mention}! Nice to see you.')
+
 
 client.run(TOKEN)
