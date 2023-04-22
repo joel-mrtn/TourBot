ed891aa5b027e75ec7e7483090866885f821fedd
Joel Martin <joel.acc@pm.me>
2023-04-22 19:30:02 +0200

    configure client to recieve and send messages

--- src/bot.py
+++ src/bot.py
@@ -3,13 +3,26 @@ import os
 import discord
 from dotenv import load_dotenv
 
+# get token from .env file
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client(intents=discord.Intents.default())
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+    # ignore self messages
+    if message.author == client.user:
+        return
+    
+    if message.content == 'foo':
+        await message.channel.send('bar')
 
 client.run(TOKEN)
