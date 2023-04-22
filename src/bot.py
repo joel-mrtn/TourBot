d36517fc9a0fe26b2243dd750ce6d102d626ee29
Joel Martin <joel.acc@pm.me>
2023-04-22 23:25:49 +0200

    add map command

--- src/bot.py
+++ src/bot.py
@@ -6,8 +6,8 @@ from dotenv import load_dotenv
 
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-MY_GUILD = discord.Object(id=os.getenv('MY_GUILD'))
+DC_TOKEN = os.getenv('DC_TOKEN')
+DC_GUILD = discord.Object(id=os.getenv('DC_GUILD'))
 
 
 class BotClient(discord.Client):
@@ -17,21 +17,44 @@ class BotClient(discord.Client):
 
     # By specifically naming the guild, the commands are updated faster on the server.
     async def setup_hook(self):
-        self.tree.copy_global_to(guild=MY_GUILD)
-        await self.tree.sync(guild=MY_GUILD)
+        self.tree.copy_global_to(guild=DC_GUILD)
+        await self.tree.sync(guild=DC_GUILD)
 
 
 intents = discord.Intents.default()
 client = BotClient(intents=intents)
 
 
+# Console log at start
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
-@client.tree.command(description='Get a nice greeting from the bot.')
+
+@client.tree.command(description='Get a nice greeting from the bot')
 async def hello(interaction: discord.Integration):
     await interaction.response.send_message(f'Hey {interaction.user.mention}! Nice to see you.')
 
 
-client.run(TOKEN)
+@client.tree.command(description='Displays the location of a point in a map')
+@app_commands.describe(
+    latitude='The latitude of the coordinate',
+    longitude='The longitude of the coordinate',
+)
+async def map(interaction: discord.Integration, latitude: float, longitude: float):
+    if latitude < -90 or latitude > 90:
+        await interaction.response.send_message("Invalid latitude value. Latitude must be between -90 and 90 degrees.")
+        return
+
+    if longitude < -180 or longitude > 180:
+        await interaction.response.send_message("Invalid longitude value. Longitude must be between -180 and 180 degrees.")
+        return
+
+    # Call function to get the map as an image
+    # Display map image
+    # Get a link to the coordinates on openstreetmap
+    # Display link as a button
+    await interaction.response.send_message(f'The coordinates are {latitude}, {longitude}')
+
+
+client.run(DC_TOKEN)
