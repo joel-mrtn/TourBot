74c0e110db5a8f54d11a59a69ca8dfa1e59321be
Joel Martin <joel.acc@pm.me>
2023-05-07 10:30:19 +0200

    change map command response to embed and add button for html map generation

--- src/bot.py
+++ src/bot.py
@@ -12,28 +12,46 @@ class BotClient(discord.Client):
         super().__init__(intents=intents)
         self.tree = app_commands.CommandTree(self)
 
+    async def on_ready(self):
+        await self.wait_until_ready()
+
+        print(f'{client.user} has connected to Discord!')
+
     # By specifically naming the guild, the commands are updated faster on the server.
     async def setup_hook(self):
         self.tree.copy_global_to(guild=DC_GUILD)
         await self.tree.sync(guild=DC_GUILD)
 
 
+class route_buttons_view(discord.ui.View):
+    def __init__(self, route: Route):
+        super().__init__(timeout=900)
+        self.route = route
+
+    @discord.ui.button(custom_id='route_html', emoji='\U0001F5FA', label='Interactive map')
+    async def route_html(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await interaction.response.send_message(
+            content="Please wait... Generating the map.",
+            ephemeral=True
+        )
+
+        await interaction.edit_original_response(
+            content=f'Download the HTML file below and open it in your web browser to see the map.',
+            attachments=[discord.File(self.route.get_html_map(), filename='map.html')]
+        )
+
+
 intents = discord.Intents.default()
 client = BotClient(intents=intents)
 
 
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
-
-
 @client.tree.command(description='Get a nice greeting from the bot')
-async def hello(interaction: discord.Integration):
+async def hello(interaction: discord.Interaction):
     await interaction.response.send_message(content=f'Hey {interaction.user.mention}! Nice to see you.', ephemeral=True)
 
 
 @client.tree.command(description='Choose addresses for the start and end')
-async def start_and_end(interaction: discord.Integration, start_addr: str, end_addr: str):
+async def start_and_end(interaction: discord.Interaction, start_addr: str, end_addr: str):
     start_coords = routes.conv_addr_to_coords(start_addr)
     end_coords = routes.conv_addr_to_coords(end_addr)
 
@@ -82,7 +100,7 @@ async def start_and_end(interaction: discord.Integration, start_addr: str, end_a
     latitude2='The latitude of the seccond coordinate',
     longitude2='The longitude of the seccond coordinate',
 )
-async def map(interaction: discord.Integration, latitude1: float, longitude1: float, latitude2: float, longitude2: float):
+async def map(interaction: discord.Interaction, latitude1: float, longitude1: float, latitude2: float, longitude2: float):
     if latitude1 < -90 or latitude1 > 90 or latitude2 < -90 or latitude2 > 90:
         await interaction.response.send_message("Invalid latitude value. Latitude must be between -90 and 90 degrees.", ephemeral=True)
         return
@@ -93,18 +111,29 @@ async def map(interaction: discord.Integration, latitude1: float, longitude1: fl
     
     # The map generation could take more than 3 secconds (which invalidates the discord token for this interaction)
     # A first response is sent to prevent this
-    await interaction.response.send_message("Please wait... Generating the map.")
+    await interaction.response.send_message("Please wait... Generating the route.")
 
     route = Route(
         route_points=[Coordinates(latitude1, longitude1), Coordinates(latitude2, longitude2)]
     )
 
-    discord_html_file = discord.File(route.get_html_map(), filename='map.html')
-    discord_png_file = discord.File(route.get_png_map(), filename='map.png')
+    overview_embed = discord.Embed(
+        title='Your route',
+        description=f'Here is the route from {latitude1}, {longitude1} to {latitude2}, {longitude2}. Open the HTML file in your browser to see the route.'
+    )
+    overview_embed.set_image(url='attachment://map.png')
+    overview_embed.set_footer(text=f'The interactive map generation will only be available the first 15 minutes and 15 minutes after the first generation.')
+
+    details_embed = discord.Embed(
+        title='Detailed info',
+        description='This is an overview of the route information (TBD).'
+    )
 
     await interaction.edit_original_response(
-        content=f'Here is the route from {latitude1}, {longitude1} to {latitude2}, {longitude2}. Open the HTML file in your browser to see the route.',
-        attachments=[discord_png_file, discord_html_file]
+        content=None,
+        embeds=[overview_embed, details_embed],
+        attachments=[discord.File(route.get_png_map(), filename='map.png')],
+        view=route_buttons_view(route)
     )
 
 
